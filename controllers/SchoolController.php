6bee866a1bd5f02d4a721b3e79a15dc4a1869794
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-08-17 22:48:09 +0700

    commit

--- controllers/SchoolController.php
+++ controllers/SchoolController.php
@@ -1,1 +1,159 @@
-/Users/putrasudaryanto/htdocs/_project_ommu_module_psb/modules/psb/controllers/SchoolController.php
+<?php
+/**
+ * SchoolController
+ * @var $this SchoolController
+ * @var $model PsbSchools 
+ * @var $form CActiveForm
+ * version: 0.0.1
+ * Reference start
+ *
+ * TOC :
+ *	Index
+ *	Suggest
+ *	AjaxGet
+ *
+ *	LoadModel
+ *	performAjaxValidation
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2016 Ommu Platform (opensource.ommu.co)
+ * @link https://github.com/ommu/PSB
+ * @contact (+62)856-299-4114
+ *
+ *----------------------------------------------------------------------------------------------------------
+ */
+
+class SchoolController extends Controller
+{
+	/**
+	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+	 * using two-column layout. See 'protected/views/layouts/column2.php'.
+	 */
+	//public $layout='//layouts/column2';
+	public $defaultAction = 'index';
+
+	/**
+	 * Initialize admin page theme
+	 */
+	public function init() 
+	{
+		$arrThemes = Utility::getCurrentTemplate('public');
+		Yii::app()->theme = $arrThemes['folder'];
+		$this->layout = $arrThemes['layout'];
+	}
+
+	/**
+	 * @return array action filters
+	 */
+	public function filters() 
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			//'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules() 
+	{
+		return array(
+			array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array('index','suggest','ajaxget'),
+				'users'=>array('*'),
+			),
+			array('allow', // allow authenticated user to perform 'create' and 'update' actions
+				'actions'=>array(),
+				'users'=>array('@'),
+				'expression'=>'isset(Yii::app()->user->level)',
+				//'expression'=>'isset(Yii::app()->user->level) && (Yii::app()->user->level != 1)',
+			),
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+				'actions'=>array(),
+				'users'=>array('admin'),
+			),
+			array('deny',  // deny all users
+				'users'=>array('*'),
+			),
+		);
+	}
+	
+	/**
+	 * Lists all models.
+	 */
+	public function actionIndex() 
+	{
+		$this->redirect(Yii::app()->createUrl('site/index'));
+	}
+	
+	/**
+	 * Updates a particular model.
+	 * If update is successful, the browser will be redirected to the 'view' page.
+	 * @param integer $id the ID of the model to be updated
+	 */
+	public function actionSuggest($limit=15) 
+	{
+		if(isset($_GET['term'])) {
+			$criteria = new CDbCriteria;
+			$criteria->condition = 'school_name LIKE :name';
+			$criteria->select = "school_id, school_name";
+			$criteria->limit = $limit;
+			$criteria->order = "school_id ASC";
+			$criteria->params = array(':name' => '%' . strtolower($_GET['term']) . '%');
+			$model = PsbSchools::model()->findAll($criteria);
+
+			if($model) {
+				foreach($model as $items) {
+					$result[] = array('id' => $items->school_id, 'value' => $items->school_name);
+				}
+			}
+		}
+		echo CJSON::encode($result);
+		Yii::app()->end();
+	}
+	
+	/**
+	 * Creates a new model.
+	 * If creation is successful, the browser will be redirected to the 'view' page.
+	 */
+	public function actionAjaxGet() 
+	{
+		if(isset($_POST['school_id'])) {
+			$model=$this->loadModel($_POST['school_id']);
+			echo CJSON::encode(array(
+				'school_name' => $model->school_name,
+				'school_address' => $model->school_address,
+				'school_phone' => $model->school_phone,
+				'school_status' => $model->school_status,
+			));
+		}
+	}
+
+	/**
+	 * Returns the data model based on the primary key given in the GET variable.
+	 * If the data model is not found, an HTTP exception will be raised.
+	 * @param integer the ID of the model to be loaded
+	 */
+	public function loadModel($id) 
+	{
+		$model = PsbSchools::model()->findByPk($id);
+		if($model===null)
+			throw new CHttpException(404, Yii::t('phrase', 'The requested page does not exist.'));
+		return $model;
+	}
+
+	/**
+	 * Performs the AJAX validation.
+	 * @param CModel the model to be validated
+	 */
+	protected function performAjaxValidation($model) 
+	{
+		if(isset($_POST['ajax']) && $_POST['ajax']==='psb-schools-form') {
+			echo CActiveForm::validate($model);
+			Yii::app()->end();
+		}
+	}
+}
--- models/PsbCourses.php
+++ models/PsbCourses.php
@@ -1,1 +1,367 @@
-/Users/putrasudaryanto/htdocs/_project_ommu_module_psb/modules/psb/models/PsbCourses.php
+<?php
+/**
+ * PsbCourses
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2016 Ommu Platform (opensource.ommu.co)
+ * @link https://github.com/ommu/PSB
+ * @contact (+62)856-299-4114
+ *
+ * This is the template for generating the model class of a specified table.
+ * - $this: the ModelCode object
+ * - $tableName: the table name for this class (prefix is already removed if necessary)
+ * - $modelClass: the model class name
+ * - $columns: list of table columns (name=>CDbColumnSchema)
+ * - $labels: list of attribute labels (name=>label)
+ * - $rules: list of validation rules
+ * - $relations: list of relations (name=>relation declaration)
+ *
+ * --------------------------------------------------------------------------------------
+ *
+ * This is the model class for table "ommu_psb_courses".
+ *
+ * The followings are the available columns in table 'ommu_psb_courses':
+ * @property string $course_id
+ * @property integer $publish
+ * @property integer $defaults
+ * @property string $course_name
+ * @property string $course_desc
+ * @property string $creation_date
+ * @property string $creation_id
+ * @property string $modified_date
+ * @property string $modified_id
+ *
+ * The followings are the available model relations:
+ * @property PsbYearCourse[] $PsbYearCourses
+ */
+class PsbCourses extends CActiveRecord
+{
+	public $defaultColumns = array();
+	
+	// Variable Search
+	public $creation_search;
+	public $modified_search;
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return PsbCourses the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'ommu_psb_courses';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('course_name', 'required'),
+			array('publish, defaults', 'numerical', 'integerOnly'=>true),
+			array('course_name', 'length', 'max'=>32),
+			array('creation_id', 'length', 'max'=>11),
+			array('course_desc', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('course_id, publish, defaults, course_name, course_desc, creation_date, creation_id, modified_date, modified_id,
+				creation_search, modified_search', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'courses' => array(self::HAS_MANY, 'PsbYearCourse', 'course_id'),
+			'creation' => array(self::BELONGS_TO, 'Users', 'creation_id'),
+			'modified' => array(self::BELONGS_TO, 'Users', 'modified_id'),
+			'view' => array(self::BELONGS_TO, 'ViewPsbCourses', 'course_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+            'course_id' => Yii::t('attribute', 'Course'),
+            'publish' => Yii::t('attribute', 'Publish'),
+            'defaults' => Yii::t('attribute', 'Default'),
+            'course_name' => Yii::t('attribute', 'Course Name'),
+            'course_desc' => Yii::t('attribute', 'Course Desc'),
+            'creation_date' => Yii::t('attribute', 'Creation Date'),
+            'creation_id' => Yii::t('attribute', 'Creation'),
+            'modified_date' => Yii::t('attribute', 'Modified Date'),
+            'modified_id' => Yii::t('attribute', 'Modified'),
+            'creation_search' => Yii::t('attribute', 'Creation'),
+            'modified_search' => Yii::t('attribute', 'Modified'),
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('t.course_id',$this->course_id,true);
+		if(isset($_GET['type']) && $_GET['type'] == 'publish')
+			$criteria->compare('t.publish',1);
+		elseif(isset($_GET['type']) && $_GET['type'] == 'unpublish')
+			$criteria->compare('t.publish',0);
+		elseif(isset($_GET['type']) && $_GET['type'] == 'trash')
+			$criteria->compare('t.publish',2);
+		else {
+			$criteria->addInCondition('t.publish',array(0,1));
+			$criteria->compare('t.publish',$this->publish);
+		}
+		$criteria->compare('t.defaults',$this->defaults);
+		$criteria->compare('t.course_name',$this->course_name,true);
+		$criteria->compare('t.course_desc',$this->course_desc,true);
+		if($this->creation_date != null && !in_array($this->creation_date, array('0000-00-00 00:00:00', '0000-00-00')))
+			$criteria->compare('date(t.creation_date)',date('Y-m-d', strtotime($this->creation_date)));
+		$criteria->compare('t.creation_id',$this->creation_id,true);
+		if($this->modified_date != null && !in_array($this->modified_date, array('0000-00-00 00:00:00', '0000-00-00')))
+			$criteria->compare('date(t.modified_date)',date('Y-m-d', strtotime($this->modified_date)));
+		$criteria->compare('t.modified_id',$this->modified_id,true);
+		
+		// Custom Search
+		$criteria->with = array(
+			'creation' => array(
+				'alias'=>'creation',
+				'select'=>'displayname'
+			),
+			'modified' => array(
+				'alias'=>'modified',
+				'select'=>'displayname'
+			),
+		);
+		$criteria->compare('creation.displayname',strtolower($this->creation_search), true);
+		$criteria->compare('modified.displayname',strtolower($this->modified_search), true);
+
+		if(!isset($_GET['PsbCourses_sort']))
+			$criteria->order = 'course_id DESC';
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+			'pagination'=>array(
+				'pageSize'=>30,
+			),
+		));
+	}
+
+
+	/**
+	 * Get column for CGrid View
+	 */
+	public function getGridColumn($columns=null) {
+		if($columns !== null) {
+			foreach($columns as $val) {
+				/*
+				if(trim($val) == 'enabled') {
+					$this->defaultColumns[] = array(
+						'name'  => 'enabled',
+						'value' => '$data->enabled == 1? "Ya": "Tidak"',
+					);
+				}
+				*/
+				$this->defaultColumns[] = $val;
+			}
+		} else {
+			//$this->defaultColumns[] = 'course_id';
+			$this->defaultColumns[] = 'publish';
+			$this->defaultColumns[] = 'defaults';
+			$this->defaultColumns[] = 'course_name';
+			$this->defaultColumns[] = 'course_desc';
+			$this->defaultColumns[] = 'creation_date';
+			$this->defaultColumns[] = 'creation_id';
+			$this->defaultColumns[] = 'modified_date';
+			$this->defaultColumns[] = 'modified_id';
+		}
+
+		return $this->defaultColumns;
+	}
+
+	/**
+	 * Set default columns to display
+	 */
+	protected function afterConstruct() {
+		if(count($this->defaultColumns) == 0) {
+			$this->defaultColumns[] = array(
+				'header' => 'No',
+				'value' => '$this->grid->dataProvider->pagination->currentPage*$this->grid->dataProvider->pagination->pageSize + $row+1'
+			);
+			$this->defaultColumns[] = array(
+				'name' => 'course_name',
+				'value' => '$data->course_name',
+			);
+			$this->defaultColumns[] = 'course_desc';
+			$this->defaultColumns[] = array(
+				'header' => 'years',
+				'value' => 'CHtml::link($data->view->years, Yii::app()->controller->createUrl("o/yearcourse/manage",array("course"=>$data->course_id)))',
+				'htmlOptions' => array(
+					'class' => 'center',
+				),
+				'type' => 'raw',
+			);
+			$this->defaultColumns[] = array(
+				'name' => 'creation_search',
+				'value' => '$data->creation->displayname',
+			);
+			$this->defaultColumns[] = array(
+				'name' => 'creation_date',
+				'value' => 'Utility::dateFormat($data->creation_date)',
+				'htmlOptions' => array(
+					'class' => 'center',
+				),
+				'filter' => Yii::app()->controller->widget('application.components.system.CJuiDatePicker', array(
+					'model'=>$this,
+					'attribute'=>'creation_date',
+					'language' => 'en',
+					'i18nScriptFile' => 'jquery-ui-i18n.min.js',
+					//'mode'=>'datetime',
+					'htmlOptions' => array(
+						'id' => 'creation_date_filter',
+					),
+					'options'=>array(
+						'showOn' => 'focus',
+						'dateFormat' => 'dd-mm-yy',
+						'showOtherMonths' => true,
+						'selectOtherMonths' => true,
+						'changeMonth' => true,
+						'changeYear' => true,
+						'showButtonPanel' => true,
+					),
+				), true),
+			);
+			if(!isset($_GET['type'])) {
+				$this->defaultColumns[] = array(
+					'name' => 'defaults',
+					'value' => 'Utility::getPublish(Yii::app()->controller->createUrl("default",array("id"=>$data->course_id)), $data->defaults, 1)',
+					'htmlOptions' => array(
+						'class' => 'center',
+					),
+					'filter'=>array(
+						1=>Yii::t('phrase', 'Yes'),
+						0=>Yii::t('phrase', 'No'),
+					),
+					'type' => 'raw',
+				);
+			}
+			if(!isset($_GET['type'])) {
+				$this->defaultColumns[] = array(
+					'name' => 'publish',
+					'value' => 'Utility::getPublish(Yii::app()->controller->createUrl("publish",array("id"=>$data->course_id)), $data->publish, 1)',
+					'htmlOptions' => array(
+						'class' => 'center',
+					),
+					'filter'=>array(
+						1=>Yii::t('phrase', 'Yes'),
+						0=>Yii::t('phrase', 'No'),
+					),
+					'type' => 'raw',
+				);
+			}
+		}
+		parent::afterConstruct();
+	}
+
+	/**
+	 * User get information
+	 */
+	public static function getInfo($id, $column=null)
+	{
+		if($column != null) {
+			$model = self::model()->findByPk($id,array(
+				'select' => $column
+			));
+			return $model->$column;
+			
+		} else {
+			$model = self::model()->findByPk($id);
+			return $model;			
+		}
+	}
+
+	/**
+	 * User get information
+	 */
+	public static function getValue($id, $value=null)
+	{
+		$course = self::model()->findByPk($id,array(
+			'select' => 'course_id, course_name',
+		));
+		$value = $value != null && $value != '' && $value != 0 ? $value : '-';
+		
+		return $course->course_name.': '.$value;
+	}
+
+	/**
+	 * Get Years
+	 */
+	public static function getCourse($default=null, $type=null) 
+	{		
+		$criteria=new CDbCriteria;
+		if($default != null)
+			$criteria->compare('defaults',$default);
+		$model = self::model()->findAll($criteria);
+
+		if($type == null) {
+			$items = array();
+			if($model != null) {
+				foreach($model as $key => $val)
+					$items[$val->course_id] = $val->course_name;
+				return $items;
+				
+			} else
+				return false;
+			
+		} else
+			return $model;
+	}
+
+	/**
+	 * before validate attributes
+	 */
+	protected function beforeValidate() {
+		if(parent::beforeValidate()) {
+			if($this->isNewRecord)
+				$this->creation_id = Yii::app()->user->id;
+			else
+				$this->modified_id = Yii::app()->user->id;				
+		}
+		return true;
+	}
+
+}
